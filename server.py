aa3bf84ba0a1ab1cf4c9b94542797d6351153044
shernix <eduardo.santiago8@gmail.com>
2020-05-21 15:47:45 -0400

    Server updated

--- server.py
+++ server.py
@@ -3,11 +3,20 @@ import socket
 host = ''
 port = 2304
 
-status = "UNKNOWN"
+
 temp = 30 # Unit: Celsius
 pH = 7.6  
 lumin = 0.1 # Unit: Lux
 press =  4  # Unit: atm
+leak = False
+
+status = "UNKNOWN"
+tempStatus = "temp OK"
+phStatus = "ph OK"
+luminStatus = "lumin OK"
+pressStatus = "press OK"
+leakStatus = "leak OK"
+
 
 def setupServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,29 +39,73 @@ def setupConnection():
     status = "OK"
     return conn
 
-def STATUS():
-    reply = status
+def STATUS(dataMessage):
+    command = dataMessage[1]
+
+    if command == "TEMPERATURE":
+        reply = tempStatus
+    elif command == "PH":
+        reply = phStatus
+    elif command == "LUMINOSITY":
+        reply = luminStatus
+    elif command == "PRESSURE":
+        reply = pressStatus
+    elif command == "LEAK":
+        reply = leakStatus
+    else:
+        reply = status
     return reply
 
-def GET(dataMessage):
-    request = dataMessage[1]
+def UPDATE():
     global temp
     global pH
     global lumin
     global press
+    global leak
 
-    if request == 'TEMPERATURE':
-        reply = temp
-    elif request == 'PH':
-        reply = pH
-    elif request == 'LUMINOSITY':
-        reply = lumin
-    elif request == 'PRESSURE':
-        reply = press
+    reply = str(temp) + ", "
+    reply = reply + str(pH) + ", "
+    reply = reply + str(lumin) + ", "
+    reply = reply + str(press) + ", "
+    reply = reply + str(leak)
+    return reply
+
+def GET(dataMessage):
+    request = dataMessage[1].split(' ')
+    global temp
+    global pH
+    global lumin
+    global press
+    global leak
+    reply = ""
+
+    if 'TEMPERATURE' in request:
+        reply = reply + str(temp)
     else:
-        reply = "Unknown parameter"
+        reply = reply + " - "
+    
+    if 'PH' in request:
+        reply = reply + "," + str(pH)
+    else:
+        reply = reply + ", - "
+    
+    if 'LUMINOSITY' in request:
+        reply = reply + "," + str(lumin)
+    else:
+        reply = reply + ", - "
+    
+    if 'PRESSURE' in request:
+        reply = reply + "," + str(press)
+    else:
+        reply = reply + ", - "
+    
+    if 'LEAK' in request:
+        reply = reply + "," + str(leak)
+    else:
+        reply = reply + ", - "
+    
 
-    return str(reply)
+    return reply
 
 def dataTransfer(conn):
     # Send/Recieve
@@ -64,7 +117,9 @@ def dataTransfer(conn):
         dataMessage = data.split(' ', 1)
         command = dataMessage[0] # command is the first segment
         if command == 'STATUS':
-            reply = STATUS()
+            reply = STATUS(dataMessage)
+        elif command == 'UPDATE':
+            reply = UPDATE()
         elif command == 'GET':
             reply = GET(dataMessage)
         elif command == 'EXIT':
